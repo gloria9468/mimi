681d46751914c057fed06c0fec6fba36815f1a3b
gloria9468 <gloria9468@naver.com>
2024-07-09 14:09:30 +0900

    mybatisConfig를 빼고 @mapper 로 db연결 해봄.

--- src/main/java/com/hello/mimi/mapper/PostMapper.java
+++ src/main/java/com/hello/mimi/mapper/PostMapper.java
@@ -1,9 +1,13 @@
 package com.hello.mimi.mapper;
 
 import com.hello.mimi.standard.post.PostDTO;
+import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 
+@Mapper
 public interface PostMapper {
+    @Select("SELECT post_id, title, body FROM post WHERE post_id = #{postId}")
     PostDTO readPost(@Param("postId") Long postId);
 }
